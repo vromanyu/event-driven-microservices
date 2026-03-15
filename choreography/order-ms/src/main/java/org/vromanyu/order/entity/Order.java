d38c00d17784f6e788a3aea6f29399b38d2a5694
vromanyu <vromanyu@outlook.com>
2026-03-15 23:31:36 +0200

    add Order entity

---
+++ choreography/order-ms/src/main/java/org/vromanyu/order/entity/Order.java
@@ -0,0 +1,101 @@
+package org.vromanyu.order.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.vromanyu.core.OrderStatus;
+import org.vromanyu.core.PaymentStatus;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+
+    @Id @GeneratedValue
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer productId;
+
+    private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Order() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", productId=" + productId +
+                ", price=" + price +
+                ", orderStatus=" + orderStatus +
+                ", paymentStatus=" + paymentStatus +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
