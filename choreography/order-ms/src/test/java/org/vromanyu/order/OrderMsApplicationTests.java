0a438e4b3910fa4608cd93798f9c8c281a358aa1
vromanyu <vromanyu@outlook.com>
2026-03-15 23:14:56 +0200

    init choreography services

---
+++ choreography/order-ms/src/test/java/org/vromanyu/order/OrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vromanyu.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
