35d7bab5f9cf2d92e74d5b9115be97bce0c96ddc
vromanyu <vromanyu@outlook.com>
2026-03-16 00:21:30 +0200

    add orderId

--- choreography/core/src/main/java/org/vromanyu/core/OrderEvent.java
+++ choreography/core/src/main/java/org/vromanyu/core/OrderEvent.java
@@ -3,6 +3,7 @@ package org.vromanyu.core;
 import java.time.LocalDate;
 
 public record OrderEvent(String eventId,
+                         Integer orderId,
                          LocalDate date,
                          OrderRequestDto orderRequestDto,
                          OrderStatus orderStatus) {
