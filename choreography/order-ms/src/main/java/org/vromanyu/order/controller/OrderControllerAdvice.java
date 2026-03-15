8d3f31b0bbc8d5691de554037b7d8e0f739b0419
vromanyu <vromanyu@outlook.com>
2026-03-16 00:22:41 +0200

    add OrderControllerAdvice

---
+++ choreography/order-ms/src/main/java/org/vromanyu/order/controller/OrderControllerAdvice.java
@@ -0,0 +1,25 @@
+package org.vromanyu.order.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class OrderControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception ex, HttpServletRequest request) {
+        Map<String, Object> response = Map.of(
+                "path", request.getRequestURI(),
+                "error", ex.getMessage(),
+                "timestamp", OffsetDateTime.now(),
+                "status", HttpStatus.BAD_REQUEST.getReasonPhrase()
+        );
+        return ResponseEntity.badRequest().body(response);
+    }
+}
