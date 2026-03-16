31cbeac07404e4ccc52d1c090c909357b8e05a26
vromanyu <vromanyu@outlook.com>
2026-03-16 12:30:59 +0200

    add method to find UserBalance by userId

--- choreography/payment-ms/src/main/java/org/vromanyu/payment/repository/UserBalanceRepository.java
+++ choreography/payment-ms/src/main/java/org/vromanyu/payment/repository/UserBalanceRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.vromanyu.payment.entity.UserBalance;
 
+import java.util.List;
+
 @Repository
 public interface UserBalanceRepository extends JpaRepository<UserBalance, Integer> {
+    List<UserBalance> findByUserId(Integer userId);
 }
