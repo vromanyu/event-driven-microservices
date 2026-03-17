1fee93a489a9958536d79e9365eac21e4baa6dbe
vromanyu <vromanyu@outlook.com>
2026-03-17 10:43:55 +0200

    fix UserInitService to populate userIds

--- choreography/payment-ms/src/main/java/org/vromanyu/payment/service/UserInitService.java
+++ choreography/payment-ms/src/main/java/org/vromanyu/payment/service/UserInitService.java
@@ -24,15 +24,19 @@ public class UserInitService implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         List<UserBalance> userBalances = new ArrayList<>();
         UserBalance userBalance = new UserBalance();
+        userBalance.setUserId(100);
         userBalance.setPrice(500);
         userBalances.add(userBalance);
         userBalance = new UserBalance();
+        userBalance.setUserId(101);
         userBalance.setPrice(1000);
         userBalances.add(userBalance);
         userBalance = new UserBalance();
+        userBalance.setUserId(102);
         userBalance.setPrice(2000);
         userBalances.add(userBalance);
         userBalance = new UserBalance();
+        userBalance.setUserId(103);
         userBalance.setPrice(3000);
         userBalances.add(userBalance);
         userBalanceRepository.saveAll(userBalances);
