d3766cae0b557d0b864bfc7c4a08870f86574d9c
vromanyu <vromanyu@outlook.com>
2026-03-17 15:33:22 +0200

    add Product repository

---
+++ cqrs/write-ms/src/main/java/org/vromanyu/write/product/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.vromanyu.write.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
