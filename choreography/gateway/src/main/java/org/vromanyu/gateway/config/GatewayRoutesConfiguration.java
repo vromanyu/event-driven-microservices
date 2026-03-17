0e261b25c373f81fa19c27f778562e4f244634fb
vromanyu <vromanyu@outlook.com>
2026-03-17 10:44:32 +0200

    break down routes to paymentRoutes and orderRoutes

--- choreography/gateway/src/main/java/org/vromanyu/gateway/config/GatewayRoutesConfiguration.java
+++ choreography/gateway/src/main/java/org/vromanyu/gateway/config/GatewayRoutesConfiguration.java
@@ -15,11 +15,21 @@ import org.springframework.web.servlet.function.ServerResponse;
 public class GatewayRoutesConfiguration {
 
     @Bean
-    public RouterFunction<ServerResponse> routes() {
+    public RouterFunction<ServerResponse> orderRoutes() {
         return RouterFunctions.route()
                 .route(RequestPredicates.path("/api/orders/**"), HandlerFunctions.http())
                 .before(BeforeFilterFunctions.addRequestHeader("X-Gateway", "true"))
                 .filter(LoadBalancerFilterFunctions.lb("order-ms"))
+                .after(AfterFilterFunctions.addResponseHeader("X-Gateway", "true"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> paymentRoutes() {
+        return RouterFunctions.route()
+                .route(RequestPredicates.path("/api/payments/**"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.addRequestHeader("X-Gateway", "true"))
+                .filter(LoadBalancerFilterFunctions.lb("payment-ms"))
                 .after(AfterFilterFunctions.addResponseHeader("X-Gateway", "true"))
                 .build();
     }
