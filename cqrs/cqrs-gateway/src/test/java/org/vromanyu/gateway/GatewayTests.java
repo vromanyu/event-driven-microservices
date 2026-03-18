b33307ea6a46c5b433141532245c39f1e55015a4
vromanyu <vromanyu@outlook.com>
2026-03-18 15:05:46 +0200

    add gateway tests

---
+++ cqrs/cqrs-gateway/src/test/java/org/vromanyu/gateway/GatewayTests.java
@@ -0,0 +1,145 @@
+package org.vromanyu.gateway;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.Body;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@EnableWireMock({@ConfigureWireMock(name = "query-ms", port = 8085), @ConfigureWireMock(name = "write-ms", port = 8086)})
+public class GatewayTests {
+
+    private final RestClient restClient = RestClient.create("http://localhost:8585");
+
+    @Autowired
+    private JsonMapper jsonMapper;
+
+    @InjectWireMock("query-ms")
+    private WireMockServer queryMsWireMock;
+
+    @InjectWireMock("write-ms")
+    private WireMockServer writeMsWireMock;
+
+    @Test
+    public void whenAllProductsAndThereAreProducts_thenReturnAllProducts() {
+        Map<String, Object> products = Map.of("products", List.of(Map.of("id", 1, "name", "cookies")));
+        queryMsWireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/products/get/"))
+                .willReturn(WireMock.jsonResponse(jsonMapper.writeValueAsString(products), HttpStatus.OK.value())));
+
+        ResponseEntity<Map<String, Object>> response = restClient.get().uri("/api/products/get/").retrieve().toEntity(new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody().get("products")).isEqualTo(List.of(Map.of("id", 1, "name", "cookies")));
+    }
+
+    @Test
+    public void whenAllProductsAndThereAreNoProducts_thenReturnEmptyList() {
+        queryMsWireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/products/get/"))
+                .willReturn(WireMock.jsonResponse(jsonMapper.writeValueAsString(Collections.emptyList()), HttpStatus.OK.value())));
+
+        ResponseEntity<List<Object>> response = restClient.get().uri("/api/products/get/").retrieve().toEntity(new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody()).isEmpty();
+    }
+
+    @Test
+    public void givenValidProductId_shouldReturnOk() {
+        Map<String, Object> product = Map.of("id", 1, "name", "cookies");
+        queryMsWireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/products/get/1")).willReturn(WireMock.jsonResponse(jsonMapper.writeValueAsString(product), HttpStatus.OK.value())));
+        ResponseEntity<Map<String, Object>> response = restClient.get().uri("/api/products/get/1").retrieve().toEntity(new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody()).isEqualTo(product);
+    }
+
+    @Test
+    public void givenInvalidProductId_shouldReturnNotFound() {
+        Map<String, Object> product = Map.of("path", "/api/products/get/1", "error", "product with id 1 not found", "timestamp", OffsetDateTime.now(), "status", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        queryMsWireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/products/get/1")).willReturn(WireMock.badRequest().withResponseBody(Body.fromJsonBytes(jsonMapper.writeValueAsBytes(product)))));
+
+        Assertions.assertThatThrownBy(() ->
+        {
+            ResponseEntity<Map<String, Object>> response = restClient.get().uri("/api/products/get/1").retrieve().toEntity(new ParameterizedTypeReference<>() {
+            });
+            Assertions.assertThat(response).isNotNull();
+            Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            Assertions.assertThat(response.getBody()).isNotNull();
+            Assertions.assertThat(response.getBody()).isEqualTo(product);
+        });
+    }
+
+    @Test
+    public void givenProductRequest_shouldReturnCreated() {
+        Map<String, Object> product = Map.of("id", 1, "name", "cookies", "price", 100, "quantity", 10);
+        Map<String, Object> productRequest = Map.of("name", "cookies", "price", 100, "quantity", 10);
+        writeMsWireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/api/products/write/"))
+                .withHeader("Content-Type", WireMock.containing(MediaType.APPLICATION_JSON_VALUE))
+                .withRequestBody(WireMock.equalToJson(jsonMapper.writeValueAsString(productRequest), true, true))
+                .willReturn(WireMock.aResponse().withStatus(201)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withHeader("Location", "/api/prodcuts/get/1").
+                        withBody(jsonMapper.writeValueAsBytes(product))));
+        ResponseEntity<Map<String, Object>> response = restClient.post().uri("/api/products/write/").body(productRequest).retrieve().toEntity(new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(response.getHeaders().getLocation()).isNotNull();
+        Assertions.assertThat(response.getHeaders().getLocation().getPath()).isEqualTo("/api/prodcuts/get/1");
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody()).isEqualTo(product);
+    }
+
+    @Test
+    public void givenProductUpdateRequest_shouldReturnUpdatedProduct() {
+        Map<String, Object> product = Map.of("id", 1, "name", "cookies", "price", 200, "quantity", 10);
+        Map<String, Object> productRequest = Map.of("name", "cookies", "price", 200, "quantity", 10);
+        writeMsWireMock.stubFor(WireMock.put(WireMock.urlEqualTo("/api/products/write/1"))
+                .withHeader("Content-Type", WireMock.containing(MediaType.APPLICATION_JSON_VALUE))
+                .withRequestBody(WireMock.equalToJson(jsonMapper.writeValueAsString(productRequest), true, true))
+                .willReturn(WireMock.aResponse().withStatus(200)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(jsonMapper.writeValueAsBytes(product))));
+        ResponseEntity<Map<String, Object>> response = restClient.put().uri("/api/products/write/1").body(productRequest).retrieve().toEntity(new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody()).isEqualTo(product);
+    }
+
+    @Test
+    public void givenProductDeleteRequest_shouldReturnNoContent() {
+        writeMsWireMock.stubFor(WireMock.delete(WireMock.urlEqualTo("/api/products/write/1")).willReturn(WireMock.noContent()));
+        ResponseEntity<Void> response = restClient.delete().uri("/api/products/write/1").retrieve().toBodilessEntity();
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+}
