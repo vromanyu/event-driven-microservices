2c0e95521f8071158cb2fdcbe866079121b558da
vromanyu <vromanyu@outlook.com>
2026-03-18 13:32:40 +0200

    init module cqrs-gateway

---
+++ cqrs/cqrs-gateway/src/test/java/org/vromanyu/gateway/CqrsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vromanyu.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
