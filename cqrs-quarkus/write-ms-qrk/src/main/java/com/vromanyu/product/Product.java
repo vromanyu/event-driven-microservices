8698f407fecf8b6cd1277418ceb9bf016a4916c4
vromanyu <vromanyu@outlook.com>
2026-04-11 01:52:38 +0300

    add Product entity and validate strategy

---
+++ cqrs-quarkus/write-ms-qrk/src/main/java/com/vromanyu/product/Product.java
@@ -0,0 +1,43 @@
+package com.vromanyu.product;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "product", schema = "write_ms_qrk")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column(name = "id")
+    Integer id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "price", nullable = false)
+    Integer price;
+
+    @Column(name = "quantity", nullable = false)
+    Integer quantity;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    OffsetDateTime updatedAt;
+
+    public Product(String name, Integer price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product() {
+    }
+}
