0dd1216b788c19f7e2747b3631ea4a7e8610ce2d
vromanyu <vromanyu@outlook.com>
2026-04-13 20:55:31 +0300

    init order-outbox module

---
+++ outbox-pattern/order-outbox/src/main/java/com/vromanyu/outbox/OrderOutboxApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.outbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOutboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderOutboxApplication.class, args);
+	}
+
+}
