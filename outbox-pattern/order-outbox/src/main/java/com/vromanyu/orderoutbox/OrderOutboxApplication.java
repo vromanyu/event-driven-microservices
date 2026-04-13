5861b9ad72ef5e26b07ebc5ddf58298aa2bdbed8
vromanyu <vromanyu@outlook.com>
2026-04-13 20:46:06 +0300

    init order-outbox microservice

---
+++ outbox-pattern/order-outbox/src/main/java/com/vromanyu/orderoutbox/OrderOutboxApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.orderoutbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOutboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderOutboxApplication.class, args);
+    }
+
+}
