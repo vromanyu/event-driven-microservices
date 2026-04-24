379a63a8f8d6ce6aca934235a9d0e960907bd85f
vromanyu <vromanyu@outlook.com>
2026-04-25 01:30:30 +0300

    write to outbox table

--- quarkus-outbox/quarkus-order-ms/src/main/java/com/vromanyu/order/OrderService.java
+++ quarkus-outbox/quarkus-order-ms/src/main/java/com/vromanyu/order/OrderService.java
@@ -2,6 +2,10 @@ package com.vromanyu.order;
 
 import com.vromanyu.dto.CreateOrderRequestDto;
 import com.vromanyu.dto.CreateOrderResponseDto;
+import com.vromanyu.event.OrderCreatedEvent;
+import com.vromanyu.outbox.Outbox;
+import com.vromanyu.outbox.OutboxDao;
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -14,11 +18,29 @@ public class OrderService {
     @Inject
     OrderDao orderDao;
 
+    @Inject
+    OutboxDao outboxDao;
+
     @Transactional
     public CreateOrderResponseDto createOrder(CreateOrderRequestDto request) {
         Order order = OrderMapper.toOrder(request);
         order.orderUuid = UUID.randomUUID().toString();
         Order createdOrder = orderDao.create(order);
+        Log.infof("order created: %s", createdOrder);
+
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent(UUID.randomUUID().toString(),
+                createdOrder.orderUuid,
+                createdOrder.itemName,
+                createdOrder.productType,
+                createdOrder.quantity,
+                createdOrder.price,
+                createdOrder.createdAt);
+        Outbox outbox = new Outbox();
+        outbox.setOrderId(createdOrder.id);
+        outbox.setProcessed(false);
+        outbox.setPayload(orderCreatedEvent);
+        outboxDao.create(outbox);
+        Log.infof("outbox created: %s", outbox);
         return OrderMapper.toOrderResponse(createdOrder);
     }
 }
